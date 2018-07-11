254842199c7abcd462bea8f1716ab6b8766dad9f
Oscar Tu <OTu@bgcengineering.ca>
2018-07-10 17:16:21 -0700

    Add player parsing

--- play_by_play.py
+++ play_by_play.py
@@ -1,16 +1,38 @@
 import pandas as pd
 
+pd.set_option('expand_frame_repr', False)
+
 # Get data into a dataframe object
 play_by_play_df = pd.read_csv('NBA Hackathon - Play by Play Data Sample (50 Games).txt', sep="\t")
 event_codes_df = pd.read_csv('NBA Hackathon - Event Codes.txt', sep="\t")
+game_lineup_df = pd.read_csv('NBA Hackathon - Game Lineup Data Sample (50 Games).txt', sep="\t")
+
 
 # Sort the events in order specified in prompt
 play_by_play_df.sort_values(['Period', 'PC_Time', 'WC_Time', 'Event_Num'], ascending=[True, False, True, True])
 
 # Create map of event codes
 #print(event_codes_df[:10])
+game_ids = play_by_play_df.Game_id.unique()
+games_list = []
+count = 1
+for game_number in game_ids:
+	player_maps = {}
+	print('GAME: ' + str(count))
+	current_game = play_by_play_df.loc[play_by_play_df['Game_id'] == game_number]
+	current_players = game_lineup_df.loc[game_lineup_df['Game_id'] == game_number]
+	input(current_players)
+	#for event in current_game:
+		# If player has +/- event in same period, perform calculation
 
-print(play_by_play_df[:10])
+	#print(games_list)
+	#input("er")
+	count += 1
+
+
+
+
+#print(play_by_play_df[:10])
 
 '''
 # Go through and parse games
